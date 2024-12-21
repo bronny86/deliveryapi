f22b69a9026c1c4999f0e40b7d6cecdb320a6577
bronny86 <bronnyhamilton@gmail.com>
2024-12-22 06:03:17 +1100

    initial setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+from init import db, ma
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config["SQLALCHEMY_DATABASE_URI"] ="os.environ.get('DATABASE_URL')"
+    
+    db.init_app(app)
+    ma.init_app(app)
+    
+    return app
